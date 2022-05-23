4d80e8b6cb6e4db1090d164d06aa11d0dc0e6356
Marcello Lopes <mrcllw@hotmail.com>
2022-05-22 22:14:56 -0400

    Added planet entity

---
+++ src/main/java/com/example/demo/models/Planet.java
@@ -0,0 +1,25 @@
+package com.example.demo.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Planet implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(nullable = false)
+  private int width;
+
+  @Column(nullable = false)
+  private int height;
+}
